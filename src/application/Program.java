5787f2b94743052ace470f4a682770075d007112
Wellington Braga <wellinbraga@gmail.com>
2022-06-09 21:59:33 -0300

    A

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department department = new Department(10, "TESTE 10");
+		
+		System.out.println(department);
+
+	}
+
+}
